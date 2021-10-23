756bbdde788cb8db140911e86081788d238fd587
Steven Enamakel <enamakel@vezures.com>
2021-10-23 09:07:10 +0300

    added pause functionality

--- contracts/ScallopToken.sol
+++ contracts/ScallopToken.sol
@@ -3,13 +3,14 @@
 pragma solidity ^0.8.0;
 
 import "./lib/ERC20.sol";
+import "./lib/Ownable.sol";
 import "./lib/Pausable.sol";
 import "./lib/ERC20Permit.sol";
 
 /**
  * Implementation of the Scallop token
  */
-contract ScallopToken is ERC20, Pausable, ERC20Permit {
+contract ScallopToken is ERC20, Pausable, ERC20Permit, Ownable {
     bool public initialized = false;
 
     constructor() {}
@@ -19,6 +20,7 @@ contract ScallopToken is ERC20, Pausable, ERC20Permit {
 
         initializeERC20("ScallopX", "SCLP");
         initializePausable();
+        initializeOwnable(owner);
         initializeERC20Permit("ScallopX");
 
         _mint(owner, 100000000 * 1e18); // mint 100 mil SCLP tokens
@@ -32,5 +34,10 @@ contract ScallopToken is ERC20, Pausable, ERC20Permit {
     ) internal virtual override {
         super._beforeTokenTransfer(from, to, amount);
         require(!paused(), "ERC20Pausable: token transfer while paused");
+    }
+
+    function togglePause() external onlyOwner {
+        if (!paused()) _pause();
+        else _unpause();
     }
 }
