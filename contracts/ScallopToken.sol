60d6b6174f48ee470036523e2abc8a773b9899c5
Steven Enamakel <enamakel@vezures.com>
2021-10-23 08:52:21 +0300

    updated contracts

--- contracts/ScallopToken.sol
+++ contracts/ScallopToken.sol
@@ -2,8 +2,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+import "./lib/ERC20.sol";
+import "./lib/Pausable.sol";
+import "./lib/ERC20Permit.sol";
 
-contract ScallopToken is ERC20PresetMinterPauser {
-    constructor() ERC20PresetMinterPauser("ScallopX", "SCLP") {}
+/**
+ * Implementation of the Scallop token
+ */
+contract ScallopToken is ERC20, Pausable, ERC20Permit {
+    bool public initialized = false;
+
+    constructor() {}
+
+    function initialize(address owner) external payable {
+        require(!initialized, "already initialized");
+
+        initializeERC20("ScallopX", "SCLP");
+        initializePausable();
+        initializeERC20Permit("ScallopX");
+
+        _mint(owner, 100000000 * 1e18); // mint 100 mil SCLP tokens
+        _unpause(); // unpause the contract
+        initialized = true;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        super._beforeTokenTransfer(from, to, amount);
+        require(!paused(), "ERC20Pausable: token transfer while paused");
+    }
 }
---
+++ scripts/deploy.js
@@ -0,0 +1,66 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+const Web3 = require('web3');
+const web3 = new Web3();
+
+async function main() {
+  // Deploy the SCLP token
+  const ScallopToken = await hre.ethers.getContractFactory("ScallopToken");
+  const token = await ScallopToken.deploy();
+  await token.deployed();
+  console.log("token deployed to:", token.address);
+
+  // set this accordingly
+  const admin = '0xd2f850606f4BB953A39a630C6505D20ab79D76A2';
+  const minter = '0xd2f850606f4BB953A39a630C6505D20ab79D76A2';
+
+  // Deploy proxy contract
+  const encodedFunctionSignature = web3.eth.abi.encodeFunctionCall({
+    name: 'initialize',
+    type: 'function',
+    inputs: [{
+        type: 'address',
+        name: 'owner'
+      }]
+    }, [minter]
+  );
+
+  const ProxyContract = await hre.ethers.getContractFactory("ScallopProxy");
+  const proxy = await ProxyContract.deploy(
+    token.address,
+    admin,
+    encodedFunctionSignature
+  );
+  await proxy.deployed();
+
+  console.log('proxy at', proxy.address);
+
+  // verify token contract
+  await hre.run("verify:verify", {
+    address: token.address,
+    constructorArguments: [],
+  });
+
+
+  await hre.run("verify:verify", {
+    address: proxy.address,
+    constructorArguments: [
+      token.address,
+      admin,
+      encodedFunctionSignature
+    ],
+  });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
