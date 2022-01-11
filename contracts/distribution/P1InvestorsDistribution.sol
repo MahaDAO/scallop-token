719e59a450cb613d6ed6460bc0a1fac62785846e
Steven Enamakel <enamakel@vezures.com>
2022-01-11 13:47:45 +0000

    added investor contracts

---
+++ contracts/distribution/P1InvestorsDistribution.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {DistributionContract} from "../lib/DistributionContract.sol";
+
+contract P1InvestorsDistribution is DistributionContract {
+    // 3 month cliff/linear release
+    constructor(address token)
+        DistributionContract(
+            token,
+            1643328000, // Jan 28th 00:00 UTC
+            90 // 3 months
+        )
+    {}
+}
