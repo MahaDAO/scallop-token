1eceb0aeea325a017805297cc0f763d68363aee4
Steven Enamakel <enamakel@mahadao.com>
2022-05-29 15:25:14 +0530

    updated comment

--- contracts/distribution/ScallopTeamDistribution.sol
+++ contracts/distribution/ScallopTeamDistribution.sol
@@ -8,7 +8,7 @@ contract ScallopTeamDistribution is DistributionContract {
     constructor(address token)
         DistributionContract(
             token,
-            1667260800, // Feb 1st 2022 00:00 UTC
+            1667260800, // Nov 1st 2022 00:00 UTC
             365 // 12 months
         )
     {}
